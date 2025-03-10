518b0fd6d28bb78f9bc7e1dcab7a46666951797e
archer <econarcher2010@gmail.com>
2025-03-10 23:05:43 +0800

    debug

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <ul v-if="todos.length > 0">
     <li v-for="todo in todos" :key="todo.id" class="flex gap-3 my-2">
       <button class="bg-red-500 text-white w-5" @click="deleteTodo(todo.id)">X</button>
-      <button class="bg-green-500 text-white w-5" @click="complete(todo.id)">V</button>
+      <button class="bg-green-500 text-white w-5" @click="completeTodo(todo.id)">V</button>
       <p :class="{ 'line-through': todo.completed }">{{ todo.text }}</p>
     </li>
   </ul>
